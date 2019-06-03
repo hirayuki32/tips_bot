6609c76e3765c53e26b5b0480f66d478e56729cd
yuki <yuki@yuki-PC.local>
2019-06-04 00:36:56 +0900

    create slackbot introduction

--- send_tips.py
+++ send_tips.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-from slackbot_settings import API_TOKE
+from slackbot_settings import API_TOKEN
 from slacker import Slacker
 
-slack = Slacker(API_TOKE)
+slack = Slacker(API_TOKEN)
 
 slack.chat.post_message("#project-飲み会", "今日はさっさと帰って飲みに行こう！",
                         as_user=True)
