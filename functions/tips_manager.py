0cd6f6a8c1082686742aa271b6e70635e6807cda
yuki <yuki@yuki-PC.local>
2019-06-16 19:05:31 +0900

    create functions when a bot called 'show tips'

--- functions/tips_manager.py
+++ functions/tips_manager.py
@@ -27,10 +27,26 @@ def update_tips(channel_id, tip_number, tip):
     _touch_tips_if_not_exist(tips_path)
 
     with open(tips_path, "r") as tips_file:
-        tips_data = json.load(tips_file)
+        tips_json = json.load(tips_file)
 
-    tips_data[tip_number] = tip
-    print(tips_data)
+    tips_json[tip_number] = tip  # set tip
 
     with open(tips_path, "w") as tips_file:
-        json.dump(tips_data, tips_file, ensure_ascii=False)
+        json.dump(tips_json, tips_file, ensure_ascii=False)
+
+
+def read_tips(channel_id):
+    """
+    Parameters
+    ----------
+    channel_id : str
+    """
+    tips_path = "tips/{}.json".format(channel_id)
+    is_exists = os.path.exists(tips_path.format(tips_path))
+    if not is_exists:
+        return "まだtipsはありません。"
+
+    with open(tips_path, "r") as tips_file:
+        tips_json = json.load(tips_file)
+    tips = json.dumps(tips_json, ensure_ascii=False, indent=2, sort_keys=True)
+    return tips
--- plugins.py
+++ plugins.py
@@ -1,6 +1,6 @@
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
-from functions.tips_manager import update_tips
+from functions.tips_manager import update_tips, read_tips
 import re
 
 
@@ -9,3 +9,10 @@ def learn_tip(message, tip_number, tip_msg):
     channel_id = message.body["channel"]
     update_tips(channel_id, tip_number, tip_msg)
     message.reply('I learned tips{} as {}'.format(tip_number, tip_msg))
+
+
+@respond_to('show tips')
+def show_tips(message):
+    channel_id = message.body["channel"]
+    tips = read_tips(channel_id)
+    message.reply(tips, in_thread=True)
